028f92ce9c301c96e36abcd7980f457f41eff34f
silvermund <cliferd@naver.com>
2021-08-26 10:22:24 +0900

    210826 v2

--- tf114/tf15_softmax.py
+++ tf114/tf15_softmax.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+
+# 1. 데이터
 tf.set_random_seed(66)
 
 x_data = [[1, 2, 1, 1],
@@ -20,30 +22,38 @@ y_data = [[0, 0, 1],        # 2
           [1, 0, 0]]        #(8, 3)
 
 # 만들어
+
+# 2. 모델링
 x = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
 y = tf.compat.v1.placeholder(tf.float32, shape=[None, 3])
 
+W = tf.Variable(tf.random.normal([4, 3]), name='weight')
+b = tf.Variable(tf.random.normal([1, 3]), name='bias')
 
-w = tf.Variable(tf.random.normal([4, 3]), name='weight')
-b = tf.Variable(tf.random.normal([3]), name='bias')
-
-hypothesis = tf.nn.softmax(tf.matmul(x, w) + b)  # softmax
+# hypothesis = tf.matmul(x, W) + b
+hypothesis = tf.nn.softmax(tf.matmul(x, W) + b) 
 
 # categorical_crossentropy
 loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(hypothesis), axis=1))
 
+
+# 3. 훈련
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 # train = optimizer.minimize(loss)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
 
+# sess = tf.compat.v1.Session()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
-sess = tf.compat.v1.Session()
-sess.run(tf.global_variables_initializer())
-
+    for step in range(2001):
+        _, cost_val = sess.run([optimizer, loss], feed_dict={x:x_data, y:y_data})
+        if step % 200 == 0:
+            print(step, cost_val)
 
 
 # 4. 평가, 예측
-results = sess.run(hypothesis, feed_dict={x:[[1, 11, 7, 9]]})
-print(results, sess.run(tf.math.argmax(results, 1)))
+    results = sess.run(hypothesis, feed_dict={x:[[1, 11, 7, 9]]})
+    print(results, sess.run(tf.math.argmax(results, 1)))
 
-sess.close()
+
